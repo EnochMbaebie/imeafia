f6ed44c546f77a0fe498ee772765c3359129b9ae
Chukwuemeka Mbaebie <chukwuemekambaebie@gmail.com>
2020-06-12 16:24:40 +0100

    naira update

--- ecommerce-front/src/core/Card.js
+++ ecommerce-front/src/core/Card.js
@@ -3,6 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
 import { addItem, updateItem, removeItem } from './cartHelpers';
+import Naira from 'react-naira';
 
 const Card = ({
   product,
@@ -99,7 +100,7 @@ const Card = ({
         {shouldRedirect(redirect)}
         <ShowImage item={product} url="product" />
         <p className="card-p  mt-2">{product.description.substring(0, 100)} </p>
-        <p className="card-p black-10">$ {product.price}</p>
+        <p className="card-p black-10"><Naira>{product.price}</Naira></p>
         <p className="black-9">Category: {product.category && product.category.name}</p>
         <p className="black-8">Added on {moment(product.createdAt).fromNow()}</p>
         {showStock(product.quantity)}
